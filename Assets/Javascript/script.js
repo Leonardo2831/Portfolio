21b5c953cbb9458622e40ea752bc1b421147e44e
Leozinho2831 <leoferraz1818@gmail.com>
2024-07-30 17:57:47 -0300

    commit: new const

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,6 +1,15 @@
+// menu
 const headerLinks = document.querySelector('.header__links');
 const menu = document.querySelector('.header__menu');
 
 menu.addEventListener('click', ()=>{
     headerLinks.classList.toggle('active');
 });
+
+// atualização gráfico
+const graphics1 = document.querySelector('.skills__graphics div:nth-of-type(1)');
+const graphics2 = document.querySelector('.skills__graphics div:nth-of-type(2)');
+const graphics3 = document.querySelector('.skills__graphics div:nth-of-type(3)');
+const graphics4 = document.querySelector('.skills__graphics div:nth-of-type(4)');
+const graphics5 = document.querySelector('.skills__graphics div:nth-of-type(5)');
+
