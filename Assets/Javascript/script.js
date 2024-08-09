be8ebb388b070f70a2088c96383ad414995b6ee7
Leozinho2831 <leoferraz1818@gmail.com>
2024-08-09 17:23:05 -0300

    commit: new media querry to js in  slide

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -6,6 +6,7 @@ menu.addEventListener('click', ()=>{
     headerLinks.classList.toggle('active');
 })
 
+// slide
 const courses = document.querySelectorAll('.slide');
 const slide = document.querySelector('.courses__slide');
 
@@ -24,15 +25,22 @@ buttonSlideRight.addEventListener('click', function(){
     // verifica se posição do slide é menor que a quantidade de cursos
     if(positionSlide <= courses.length){
         // cada else é um tamanho de tela
-        if(widthScreen < 768){
-            slide.style.cssText = `transform: translateX(${-340 * (positionSlide - 1)}px)`;
+        if(widthScreen < 480){
+            slide.style.cssText = `transform: translateX(${-260 * (positionSlide - 1)}px)`;
             // como a posição inicial é uma, para contas foi necessário tirar 1 da posição
+
+        } else if(widthScreen >= 480 && widthScreen < 768){
+            slide.style.cssText = `transform: translateX(${-420 * (positionSlide - 1)}px)`;
+
         } else if(widthScreen >= 768 && widthScreen < 1024){
-            slide.style.cssText = `transform: translateX(${-600 * (positionSlide - 1)}px)`;
+            slide.style.cssText = `transform: translateX(${-630 * (positionSlide - 1)}px)`;
+
         } else if(widthScreen >= 1024){
             slide.style.cssText = `transform: translateX(${-900 * (positionSlide - 1)}px)`;
+
         }
         // caso a posição do slide for maior que o curso, irá resetar, voltar do início
+
     } else if(positionSlide > courses.length){
         positionSlide = 1;
         slide.style.cssText = `transform: translateX(0);`;
@@ -44,21 +52,37 @@ buttonSlideLeft.addEventListener('click', function(){
     // valor negativo para retroceder 1
 
     if(positionSlide <= courses.length && positionSlide > 0){
-        if(widthScreen < 768){
-            slide.style.cssText = `transform: translateX(${-340 * (positionSlide - 1)}px);`;
+
+        if(widthScreen < 480){
+            slide.style.cssText = `transform: translateX(${-260 * (positionSlide - 1)}px)`;
+            // como a posição inicial é uma, para contas foi necessário tirar 1 da posição
+
+        } else if(widthScreen >= 480 && widthScreen < 768){
+            slide.style.cssText = `transform: translateX(${-420 * (positionSlide - 1)}px)`;
+
         } else if(widthScreen >= 768 && widthScreen < 1024){
-            slide.style.cssText = `transform: translateX(${-600 * (positionSlide - 1)}px);`;
+            slide.style.cssText = `transform: translateX(${-630 * (positionSlide - 1)}px);`;
+
         } else if(widthScreen >= 1024){
             slide.style.cssText = `transform: translateX(${-900 * (positionSlide - 1)}px);`;
+
         }
+
     } else if(positionSlide < 1){
         // caso a posição for menor que 1 ele vai para o final 
-        if(widthScreen < 768){
-            slide.style.cssText = `transform: translateX(${-340 * (courses.length - 1)}px);`;
+        if(widthScreen < 480){
+            slide.style.cssText = `transform: translateX(${-260 * (courses.length - 1)}px)`;
+            // como a posição inicial é uma, para contas foi necessário tirar 1 da posição
+
+        } else if(widthScreen >= 480 && widthScreen < 768){
+            slide.style.cssText = `transform: translateX(${-420 * (courses.length - 1)}px)`;
+
         } else if(widthScreen >= 768 && widthScreen < 1024){
-            slide.style.cssText = `transform: translateX(${-600 * (courses.length - 1)}px);`;
+            slide.style.cssText = `transform: translateX(${-630 * (courses.length - 1)}px);`;
+
         } else if(widthScreen >= 1024){
             slide.style.cssText = `transform: translateX(${-900 * (courses.length - 1)}px);`;
+
         }
 
         positionSlide = 3;
