983811ee86efb4bb76f5604f4f5cc9d68ae7d096
Leonardo2831 <leoferraz1818@gmail.com>
2026-08-19 14:41:04 -0300

    remove graphics and cousers

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -39,58 +39,6 @@ function showSectionAnimation(){
 }
 showSectionAnimation();
 
-function slide(){
-    const courses = document.querySelectorAll('.js-slide');
-    const slide = document.querySelector('.js-courses');
-    
-    const buttonSlideLeft = document.querySelector('.js-coursesMain div img:first-of-type');
-    const buttonSlideRight = document.querySelector('.js-coursesMain div img:last-of-type');
-    
-    const frame = document.querySelector('.js-coursesMain div');
-    const widthFrame = parseFloat(getComputedStyle(frame).width);
-
-    if(courses && slide && buttonSlideLeft && buttonSlideRight && frame){
-        // adicionando o width do tamanho do frame para cada slide
-        courses.forEach(course => {
-            course.style.width = `${widthFrame}px`;
-        });
-
-        // posição inicial
-        let positionSlide = 1;
-
-        buttonSlideRight.addEventListener('click', function(){
-            // aumenta 1 a cada clique 
-            positionSlide++;
-
-            // verifica se posição do slide é menor que a quantidade de cursos
-            // caso a posição do slide for maior que o curso, irá para else if, resetando ao estilo normal
-            if(positionSlide <= courses.length){
-                slide.style.cssText = `transform: translateX(${-widthFrame * (positionSlide - 1)}px); transition: all 0.3s ease-in;`;
-
-            } else if(positionSlide > courses.length){
-                positionSlide = 1;
-                slide.style.cssText = `transform: translateX(0);`;
-            }
-        });
-
-        buttonSlideLeft.addEventListener('click', function(){
-            // diminui 1 a cada clique
-            positionSlide--;
-
-            // verifica se a posição do slide é menor que a quantidade de cursos e se a posição do slide maior que 0
-            // caso não ser e a posição for menor que 1 ele vai para o final
-            if(positionSlide <= courses.length && positionSlide > 0){
-                slide.style.cssText = `transform: translateX(${-widthFrame * (positionSlide - 1)}px); transition: all 0.3s ease-in;`;
-            } else if(positionSlide < 1){
-                slide.style.cssText = `transform: translateX(${-(widthFrame * (courses.length - 1))}px);`;
-
-                positionSlide = courses.length;
-            }
-        });
-    }
-}
-slide();
-
 function scrollBehaviorSmooth(){
     const headerLinks = document.querySelectorAll('.js-scrollSmooth a[href^="#"]');
 
